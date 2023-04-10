2a6f87446e50880f51528b4aa3573793afa53331
william da cunha <79292269+williamdcunha@users.noreply.github.com>
2023-04-10 19:38:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { useState } from 'react';
+import { Perguntas } from '../../questions/perguntas';
+import ReloadButton from 'handleClick'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />    
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+export default function Quiz() {
+  const questions = Perguntas ?? []
+  const [perguntaAtual, setPerguntaAtual] = useState(0);
+  const [showPontuacao, setShowPontuacao] = useState(false);
+  const [pontos, setPontos] = useState(0);
+
+  function proximaPergunta(correta) {
+    const nextQuestion = perguntaAtual + 1;
+
+    if (correta){
+      setPontos(pontos + 1);
+    }
+
+    if (nextQuestion < questions.length) {
+      setPerguntaAtual(nextQuestion);
+    } else {
+      setShowPontuacao(true);
+    }
+  }
+
+  return (
+    <div className='container'>
+      {showPontuacao ? (
+        <div className='pontuacao'>
+          <span>Sua pontuação é {pontos} de {questions.length}</span>
+            </ReloadButton>
+        </div>) : (
+        <>
+          <div className='infoPerguntas'>
+            <div className="contagemPerguntas">
+              <span>Pergunta {perguntaAtual + 1}/{questions.length}</span>
+            </div >
+            <div className="pergunta">{questions[perguntaAtual].pergunta}</div>
+          </div >
+          <div className="resposta">
+            {questions[perguntaAtual].opcoesResposta.map((opcoesResposta) =>
+              <div className="grupoResposta">
+                <span>{opcoesResposta.alternativa}</span>
+                <button onClick={() => proximaPergunta(opcoesResposta.correta)}>{opcoesResposta.resposta}</button>
+              </div>)}
+          </div>
+        </>
+      )}
+    </div >
+  );
+}
